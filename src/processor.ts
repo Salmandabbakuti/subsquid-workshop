8fdc46a56500ef3f7c8b94713a2cdef94acaa7ae
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-09-26 17:57:33 +0530

    updated: changes related to polygon usdc

--- src/processor.ts
+++ src/processor.ts
@@ -1,41 +1,41 @@
-import {lookupArchive} from '@subsquid/archive-registry'
+import { lookupArchive } from "@subsquid/archive-registry";
 import {
-    BlockHeader,
-    DataHandlerContext,
-    EvmBatchProcessor,
-    EvmBatchProcessorFields,
-    Log as _Log,
-    Transaction as _Transaction,
-} from '@subsquid/evm-processor'
-import {Store} from '@subsquid/typeorm-store'
-import * as erc20 from './abi/erc20'
+  BlockHeader,
+  DataHandlerContext,
+  EvmBatchProcessor,
+  EvmBatchProcessorFields,
+  Log as _Log,
+  Transaction as _Transaction
+} from "@subsquid/evm-processor";
+import { Store } from "@subsquid/typeorm-store";
+import * as erc20 from "./abi/erc20";
 
-export const CONTRACT_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+export const CONTRACT_ADDRESS = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        archive: lookupArchive('eth-mainnet'),
-        chain: 'https://rpc.ankr.com/eth',
-    })
-    .setFinalityConfirmation(10)
-    .setFields({
-        log: {
-            topics: true,
-            data: true,
-        },
-        transaction: {
-            hash: true,
-        },
-    })
-    .addLog({
-        range: {from: 6_082_465},
-        address: [CONTRACT_ADDRESS],
-        topic0: [erc20.events.Transfer.topic],
-        transaction: true,
-    })
+  .setDataSource({
+    archive: lookupArchive("polygon"),
+    chain: "https://polygon-rpc.com"
+  })
+  .setFinalityConfirmation(10)
+  .setFields({
+    log: {
+      topics: true,
+      data: true
+    },
+    transaction: {
+      hash: true
+    }
+  })
+  .addLog({
+    range: { from: 47854776 },
+    address: [CONTRACT_ADDRESS],
+    topic0: [erc20.events.Transfer.topic],
+    transaction: true
+  });
 
-export type Fields = EvmBatchProcessorFields<typeof processor>
-export type Context = DataHandlerContext<Store, Fields>
-export type Block = BlockHeader<Fields>
-export type Log = _Log<Fields>
-export type Transaction = _Transaction<Fields>
+export type Fields = EvmBatchProcessorFields<typeof processor>;
+export type Context = DataHandlerContext<Store, Fields>;
+export type Block = BlockHeader<Fields>;
+export type Log = _Log<Fields>;
+export type Transaction = _Transaction<Fields>;
